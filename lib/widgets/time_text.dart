3677f87a143299be4dc76b89a7eced04337f2b39
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-12-13 10:46:13 +0000

    Create time_text.dart

---
+++ lib/widgets/time_text.dart
@@ -0,0 +1,40 @@
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:travel_animation/providers/page_offset_provider.dart';
+import 'package:travel_animation/utils/app_colors.dart';
+
+import '../utils/constants.dart';
+
+class TimeText extends StatelessWidget {
+  const TimeText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PageOffsetProvider>(
+      builder: (context, notifier, child) {
+        double opacity = math.max(0, 4 * notifier.page - 3);
+        return Positioned(
+          top: topMargin(context) + mainSquareSize(context) + 32 + 16 + 32 + 40,
+          width: (MediaQuery.of(context).size.width - 48) / 3,
+          left: opacity * 24.0,
+          child: Opacity(
+            opacity: opacity,
+            child: child,
+          ),
+        );
+      },
+      child: const Align(
+        alignment: Alignment.centerRight,
+        child: Text(
+          '02:40 pm',
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w300,
+            color: AppColors.lighterGrey,
+          ),
+        ),
+      ),
+    );
+  }
+}
